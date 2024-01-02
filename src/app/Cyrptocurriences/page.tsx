eec44127a8f1467af2b3f5ca095e07b3806e57b2
SriSomanaath <govindalapudisrinath@gmail.com>
2024-01-02 11:28:46 +0530

    err

--- src/app/Cyrptocurriences/page.tsx
+++ src/app/Cyrptocurriences/page.tsx
@@ -16,20 +16,19 @@ const Cryptocurrencies = ({simplified}:any) => {
   return (
     <div className="m-10">
       <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-5 lg:mt-10 lg:mb-10">
-      {cryptos && cryptos.map((currency, index) => (
-  <div key={currency.id || index} className="relative w-100 h-100 bg-gray-300 p-8 rounded-lg mb-4">
-    <div className="flex flex-row justify-between items-start absolute top-0 left-0 right-0 p-4">
-      <h5 className="mb-8 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        {currency.name}
-      </h5>
-      <img className="w-8 h-8" src={currency.iconUrl} alt={currency.name} />
-    </div>
-    <p className="mt-16">Price: {millify(currency.price)}</p>
-    <p>Market Cap: {millify(currency.marketCap)}</p>
-    <p>Daily Change: {currency.change}%</p>
-  </div>
-))}
-
+      {cryptos && cryptos.map((currency:any, index) => (
+          <div key={currency.id || index} className="relative w-100 h-100 bg-gray-300 p-8 rounded-lg mb-4">
+            <div className="flex flex-row justify-between items-start absolute top-0 left-0 right-0 p-4">
+              <h5 className="mb-8 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {currency.name}
+              </h5>
+              <img className="w-8 h-8" src={currency.iconUrl} alt={currency.name} />
+            </div>
+            <p className="mt-16">Price: {millify(currency.price)}</p>
+            <p>Market Cap: {millify(currency.marketCap)}</p>
+            <p>Daily Change: {currency.change}%</p>
+          </div>
+        ))}
       </div>
     </div>
   );
