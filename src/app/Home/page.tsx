622cfafb345e37b586e1d3857bed225e4a2a7fe2
SriSomanaath <govindalapudisrinath@gmail.com>
2024-01-02 11:49:45 +0530

    err

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -34,34 +34,35 @@ const Home = () => {
     <div>
       <h1 className="text-bold text-6xl m-10 text-gray-700">Global Crypto Stats</h1>
       <div className="flex flex-row justify-around">
-        {globalStats ? (
-          <div className="flex flex-col">
-            <div className="flex flex-col m-5">
-              <span>Total Cryptocurrencies</span>
-              <span>{globalStats.total}</span>
-            </div>
-            <div className="flex flex-col m-5">
-              <span>Total Market Cap</span>
-              <span>{`$${millify(globalStats.total24hVolume)}`}</span>
-            </div>
-          </div>
-        ) : (
-          <p>Loading...</p>
-        )}
-        {globalStats ? (
-          <div className="flex flex-col">
-            <div className="flex flex-col m-5">
-              <span>Total Cryptocurrencies</span>
-              <span>{millify(globalStats.totalMarkets)}</span>
-            </div>
-            <div className="flex flex-col m-5">
-              <span>Total Market Cap</span>
-              <span>{`$${millify(globalStats.totalMarketCap)}`} </span>
-            </div>
-          </div>
-        ) : (
-          <p>Loading...</p>
-        )}
+      {globalStats ? (
+  <div className="flex flex-col">
+    <div className="flex flex-col m-5">
+      <span>Total Cryptocurrencies</span>
+      <span>{globalStats.data.stats.total}</span>
+    </div>
+    <div className="flex flex-col m-5">
+      <span>Total Market Cap</span>
+      <span>{`$${millify(globalStats.data.stats.total24hVolume)}`}</span>
+    </div>
+  </div>
+) : (
+  <p>Loading...</p>
+)}
+{globalStats ? (
+  <div className="flex flex-col">
+    <div className="flex flex-col m-5">
+      <span>Total Cryptocurrencies</span>
+      <span>{millify(globalStats.data.stats.totalMarkets)}</span>
+    </div>
+    <div className="flex flex-col m-5">
+      <span>Total Market Cap</span>
+      <span>{`$${millify(globalStats.data.stats.totalMarketCap)}`} </span>
+    </div>
+  </div>
+) : (
+  <p>Loading...</p>
+)}
+
       </div>
           <div className="flex justify-between p-6 max-sm:p-2">
             <h1 className="text-2xl font-bold">Top 10 Cyrptocurriences in the world</h1>
