4e108d0e7805cf5ea2094bfd3f3d979d1ad4b7af
Sreenath <srinath@techarion.com>
2024-02-09 18:25:26 +0530

    adj

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className="flex justify-between bg-major1 p-3">
-      <div className=" text-3xl font-bold text-major2">Tech Arion</div>
+      <div className=" text-3xl font-bold text-major2">Crypto Web</div>
       <span className="text-md font-bold pt-1">LogIn/SignUp</span>
     </div>
   )
